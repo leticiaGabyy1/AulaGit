fcae32229fe9cecbec41da1b7cb79e4ee615636a
leticia <leticia12112008@gmail.com>
2025-04-07 15:42:23 -0300

    Testes no código

--- Git.js
+++ Git.js
@@ -3,6 +3,7 @@ console.log(`lista de todas as comidas ${comidas}`);
 console.log(`primeira comida ${comidas[0]}`);
 comidas.short();
 console.log(`comidas ordenadas ${comidas}`);
+console.log('FIM');
 
 
 
