5df87f83387e1afd7ac6d61d8152980dc60f795e
leticia <leticia12112008@gmail.com>
2025-04-07 15:18:04 -0300

    Incluido ordenação da lista

--- Git.js
+++ Git.js
@@ -1,4 +1,8 @@
 let comidas = ['lasanha', 'pizza','sushi','hamburguer'];
 console.log(`lista de todas as comidas ${comidas}`);
 console.log(`primeira comida ${comidas[0]}`);
+comidas.short();
+console.log(`comidas ordenadas ${comidas}`);
+
+
 
